d5c5aea894e6d6fe99c598bec779762c2cd88320
adamkarminski <adamkarminski@gmail.com>
2023-12-01 10:35:48 +0100

    restore link to local link image file

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
 export const SITE_TITLE = 'Bethink Group - E-learning reimagined';
 export const SITE_DESCRIPTION =
 	'Bethink runs multiple disruptive e-learning brands. We cover medicine, med-prep and effective learning, using our custom technology.';
-export const SITE_LINK_IMAGE =
-	'https://res.cloudinary.com/bethink/image/upload/v1701423188/bethinkgroup__link_image_ewmsdf.png';
+export const SITE_LINK_IMAGE = '/bethinkgroup__link_image.png';
