190613e1b2404fac8afc741158b9c1cd02c410f0
adamkarminski <adamkarminski@gmail.com>
2023-12-01 10:35:12 +0100

    update site url in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-import vue from "@astrojs/vue";
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), vue()]
+	site: 'https://bethinkgroup.com',
+	integrations: [mdx(), sitemap(), tailwind(), vue()],
 });
